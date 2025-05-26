0c079a4d8aa379645f03f408181752e0e9dba568
Marcelo Santos <marcelo.saantoos@hotmail.com>
2025-05-26 18:45:48 -0300

    input

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import { Form, Link } from "react-router-dom";
 
 const LoginForm = () => {
+    const [userName, setUserName] = React.useState('');
+    const [password, setpassword] = React.useState('');
     return (
         <section>
             <h1>Login</h1>
             <form action=""> 
-                <input type="text"/>
+                <input 
+                    type="text"
+                    value={userName}
+                    onChange={({target})=> setUserName(target.value)}
+                />
+                <input 
+                    type="text"
+                    value={password}
+                    onChange={({target})=> setpassword(target.value)}
+                />
             </form>
             <Link to="/login/criar">
                 Cadastro
